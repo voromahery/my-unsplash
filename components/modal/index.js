4421aa3695c551c8154ed5ab6f93095280df3582
Developer <daniel.her@onja.org>
2022-11-11 14:23:13 +0300

    Add modal

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,1 +1,14 @@
-import "./index.scss"
+import styles from "./Modal.module.scss";
+
+const Modal = ({ title = "test", children }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <h3 className={styles.title}>{title}</h3>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
