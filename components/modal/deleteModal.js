bb5f210ff03942ce4ed29d5dcbb8768e25fb82e5
Developer <daniel.her@onja.org>
2022-11-12 09:49:57 +0300

    Add action

--- components/modal/deleteModal.js
+++ components/modal/deleteModal.js
@@ -2,8 +2,11 @@ import styles from "./Modal.module.scss";
 import ModalWrapper from "./modalWrapper";
 import Form from "../form/index";
 import Button from "../buttons/index";
+import { useDispatch } from 'react-redux';
+import { displayModal } from '../../store/actions/index';
 
 const DeleteModal = () => {
+  const dispatch = useDispatch()
   return (
     <ModalWrapper
       title="Are you sure?"
@@ -12,7 +15,7 @@ const DeleteModal = () => {
           <Form type="password" placeholder="******************" />
           <div className={styles.buttonWrapper}>
             <Button type="delete" label="Delete" />
-            <Button type="cancel" label="Cancel" />
+            <Button type="cancel" label="Cancel" action={() => dispatch(displayModal(false))} />
           </div>
         </>
       }
